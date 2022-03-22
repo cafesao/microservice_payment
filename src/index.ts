152201a092abc069dc43d95893720ef2733f0278
cafesao <49770350+cafesao@users.noreply.github.com>
2022-03-22 14:47:16 -0300

    Feat: Add base code

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import cors from 'cors'
+import routes from './routes/routes'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const server = express()
+
+server.use(express.json())
+server.use(cors())
+
+server.use('/payment', routes)
+
+server.listen(process.env.PORT_API, () =>
+  console.log(`Server start in port ${process.env.PORT_API}`),
+)
